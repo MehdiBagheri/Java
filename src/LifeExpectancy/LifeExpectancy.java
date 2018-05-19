7d28905c2ed24a4db09422f5f56b7e14bc386186
MehdiBagheri <bagherii.mahdii@gmail.com>
2018-05-19 17:48:51 +0200

    LifeExpectancy

---
+++ src/LifeExpectancy/LifeExpectancy.java
@@ -0,0 +1,81 @@
+package LifeExpectancy;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LifeExpectancy extends PApplet {
+
+    UnfoldingMap map;
+
+    Map<String, Float> lifeExpByCountry;
+
+    List<Feature> countries;
+    List<Marker> countryMarkers;
+    private Map<String, Float>
+        loadLifeExpectancyFromCSV(String fileName){
+        Map<String, Float> lifeExpMap
+                = new HashMap<String, Float>(); // Constructor
+
+        String[] rows = loadStrings(fileName); //Read file
+
+        for (String row:rows){
+            String[] columns = row.split(","); //split base on ","
+            float value = Float.parseFloat(columns[5]);//Life expectancy
+            lifeExpMap.put(columns[4], value);//County
+        }
+
+        return lifeExpMap;
+    }
+
+    private void shadeCountries(){
+        for(Marker marker : countryMarkers){
+            String countryId = marker.getId();
+
+            if(lifeExpByCountry.containsKey(countryId)){
+                float lifeExp = lifeExpByCountry.get((countryId));
+                int colorLevel = (int) map(lifeExp, 40, 90, 10, 255);
+                marker.setColor(color(255-colorLevel,100,colorLevel));
+
+            }
+            else{
+                marker.setColor(color(150,150,150));
+            }
+        }
+    }
+    public void setup(){
+        size(800, 600, OPENGL);
+        map =new UnfoldingMap(this,
+                50,
+                50,
+                700,
+                500,
+                new Google.GoogleMapProvider());
+        MapUtils.createDefaultEventDispatcher(this, map);
+
+        lifeExpByCountry = loadLifeExpectancyFromCSV(
+                "LifeExpectancyWorldBank.csv"
+        );
+
+        countries = GeoJSONReader.loadData(this,
+                "countries.geo.json");
+        countryMarkers = MapUtils.createSimpleMarkers(countries);
+
+        map.addMarkers(countryMarkers);
+        shadeCountries();
+
+    }
+
+    public void draw(){
+        map.draw();
+    }
+}
